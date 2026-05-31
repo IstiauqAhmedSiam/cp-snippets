c2d8596110f2ba4577b8ed9830f751fab478dbc7
Istiauq Ahmed Siam <istiauq.ahmed.siam@gmail.com>
2026-05-31 23:13:56 +0600

    updated code

--- template.cpp
+++ template.cpp
@@ -1,14 +1,50 @@
 #include <bits/stdc++.h>
-#define int long long
 using namespace std;
 
-int32_t main(){
-    int tt;
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define nline '\n'
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define dbg(x) cerr << #x <<" = "; _print(x); cerr << endl;
+#else
+#define dbg(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.first); cerr << ","; _print(p.second); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+void solve(){
+    
+}
+
+int main(){
+    fastio();
+
+    int tt = 1;
     cin >> tt;
 
     while(tt--){
-        
+        solve();
     }
-    
+
     return 0;
-}
+}   
